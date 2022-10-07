6512a99f637b4251480d4887bafeb2c2fef70e59
Mahi <mahichowdhury587@gmail.com>
2022-10-07 22:17:42 +0600

    Improved Card section and removed overflow

--- src/MyComponents/Studies/Studies.jsx
+++ src/MyComponents/Studies/Studies.jsx
@@ -6,12 +6,12 @@ import plainst from './../../images/plainst.png'
 function Studies() {
     return (
         <div className='bg-gray-100 mt-10 flex flex-col items-center'>
-            <div>
+            <div className='lg:ml-[16rem] lg:w-full'>
             <p className='text-3xl font-[1000] font-sans mt-20 mr-16 md:mr-[60rem] md:text-4xl'>Our case studies</p>
             <p className='hover:text-red-400 cursor-pointer mr-40 mt-16'>|  &nbsp;&nbsp;&nbsp;&nbsp;See all projects</p>
             </div>
-            <div className='md:flex cursor-pointer justify-evenly -mt-12 md:mt-2'>
-            <div className="w-80 bg-[#ffffff] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
+            <div className='md:flex cursor-pointer justify-evenly -mt-12 lg:mt-[1.5rem] md:mt-2'>
+            <div className="w-80 bg-[#ffffff] lg:w-[18rem] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
                 <img className="w-full"
                     src={studies}
                     alt="studies"/>
@@ -20,7 +20,7 @@ function Studies() {
                     <p>This website achieved Google ranking in four months: Ranks #1-#3 for keyword</p>
                 </div>
             </div>
-            <div className="hidden md:block w-80 bg-[#ffffff] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
+            <div className="hidden md:block w-80 bg-[#ffffff] lg:w-[18rem] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
                 <img className="w-full"
                     src={tremely}
                     alt="tremely"/>
@@ -29,7 +29,7 @@ function Studies() {
                     <p>This sire applies a smart SEO strategy to acquire online clients via long-tail search...</p>
                 </div>
             </div>
-            <div className="hidden md:block w-80 bg-[#ffffff] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
+            <div className="hidden md:block w-80 bg-[#ffffff] lg:w-[18rem] md:w-96 md:h-3/5 rounded-3xl overflow-hidden  md:mt-0 shadow-xl md:mr-10 mt-14 hover:shadow-2xl bg-scroll mb-20">
                 <img className="w-full"
                     src={plainst}
                     alt="plainist"/>
